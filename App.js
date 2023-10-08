df1ddb6b75358407467d36cbface55b69bee3ab1
rpostulart <ramonpostulart@gmail.com>
2023-10-08 21:18:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { View, Image, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
+import { Amplify, API, Auth, Analytics } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
 
-export default function App() {
+import { toByteArray } from "react-native-quick-base64";
+
+import {
+  Predictions,
+  AmazonAIPredictionsProvider,
+} from "@aws-amplify/predictions";
+
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure(awsconfig);
+Analytics.disable();
+Predictions.addPluggable(new AmazonAIPredictionsProvider());
+
+const CameraComponent = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [imageUri, setImageUri] = useState(null);
+  const cameraRef = useRef(null); // Ref for the camera component
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const resizeImage = async (uri, width, height) => {
+    const resizedPhoto = await manipulateAsync(
+      uri,
+      [{ resize: { width, height } }],
+      { format: SaveFormat.JPEG, compress: 0.6, base64: true }
+    );
+    return resizedPhoto;
+  };
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.7, base64: true };
+      const photo = await cameraRef.current.takePictureAsync(options);
+
+      const bytes = await toByteArray(photo.base64);
+      console.log(photo.base64);
+
+      try {
+        await Predictions.identify({
+          text: {
+            source: { bytes },
+          },
+        });
+      } catch (error) {
+        console.log("AAA", error);
+      }
+    }
+  };
+
+  const refocusCamera = () => {
+    // Refocus the camera by taking another picture
+    takePicture();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Camera
+        ref={cameraRef} // Set the camera ref
+        style={{ flex: 1 }}
+        type={cameraType}
+      >
+        {/* Circular button for refocusing */}
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            backgroundColor: "white",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onPress={refocusCamera}
+        >
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              backgroundColor: "#E97E91",
+            }}
+          />
+        </TouchableOpacity>
+      </Camera>
+      {imageUri && <Image source={{ uri: imageUri }} style={{ flex: 1 }} />}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default withAuthenticator(CameraComponent);
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "testcameraa68b7e89": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "identifyText4d315636": {
+      "format": "string",
+      "region": "string"
+    }
+  }
+}
